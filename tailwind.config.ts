b1e6b16308f0f61401944af703c7a35a656410ca
fordanedward <202210106@gordoncollege.edu.ph>
2024-10-04 14:19:18 +0800

    initial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,30 @@ import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
-
+	content: ["./src/**/*.{html,js,svelte,ts}", "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"],
+	darkMode: "selector",
   theme: {
     extend: {
       fontFamily: {
         sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
-      }
-    }
-  },
-
-  plugins: []
-} as Config;
+      },
+      colors: {
+          // flowbite-svelte
+          primary: {
+            "50": "#fef2e0",
+            "100": "#fedeb1",
+            "200": "#fcc97f",
+            "300": "#fbb24d",
+            "400": "#faa227",
+            "500": "#f99206",
+            "600": "#f58606",
+            "700": "#ef7706",
+            "800": "#e96706",
+            "900": "#df4c05",
+          },
+        },
+      },
+    },
+  
+      plugins: [],
+  } as Config;
